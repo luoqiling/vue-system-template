3a28c2308500cde0d8af4d9290cdf877b9a0d56e
luobin <luobin@nfmedia.com>
2020-01-16 10:37:08 +0800

    删除冗余代码

--- src/utils/index.js
+++ src/utils/index.js
@@ -76,26 +76,6 @@ export function formatTime(time, option) {
 }
 
 /**
- * @param {string} url
- * @returns {Object}
- */
-export function param2Obj(url) {
-  const search = url.split('?')[1]
-  if (!search) {
-    return {}
-  }
-  return JSON.parse(
-    '{"' +
-    decodeURIComponent(search)
-      .replace(/"/g, '\\"')
-      .replace(/&/g, '","')
-      .replace(/=/g, '":"')
-      .replace(/\+/g, ' ') +
-    '"}'
-  )
-}
-
-/**
  * 深拷贝数组
  * @param {Array} arr
  * @returns {Array}
